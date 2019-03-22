0c64949a0ca44424be69f4c24284854ca15e1e2b
Biav <beatrizvieirasilva.v@gmail.com>
2019-03-21 21:33:23 -0300

    Add new quote

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <new-quote @addQuote="addQuote"></new-quote>
+    </div>
+</template>
+
+<script>
+    import NewQuote from "./NewQuote.vue";
+
+    export default {
+        data: function() {
+            return {
+                quotes: ["My First quote"]
+            }
+        },
+        methods: {
+            addQuote(quote) {
+                this.quotes.push(quote);
+            }
+        },
+        components: {
+             'new-quote': NewQuote 
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ src/NewQuote.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <form class="form-group">
+
+            <textarea class="col-md-6" v-model="quote">
+            </textarea>
+
+            <button class="btn btn-primary col-md-6" @click.prevent="addQuote">
+                Add quote
+            </button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                quote: ''
+            }
+        },
+        methods: {
+            addQuote() {
+                this.$emit("addQuote", this.quote);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    form {
+        display: flex;
+        align-items: center;
+        flex-flow: column;
+        margin-top: 20px;
+    }
+
+    textarea {
+        height: 100px;
+        resize: none;
+    }
+</style>
